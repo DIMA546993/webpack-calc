189d009bb8c016d07aa17883b8011eb6349814e2
Dmitriy Svyatoho <dmitrijsvatoho6@gmail.com>
2026-08-12 20:55:26 +0300

    Updates

---
+++ print.bundle.js
@@ -0,0 +1,21 @@
+"use strict";
+(self["webpackChunklesson47_webpack"] = self["webpackChunklesson47_webpack"] || []).push([[568],{
+
+/***/ 258
+() {
+
+function printMe() {
+  console.log("I get called from print.js!");
+}
+
+printMe();
+
+/***/ }
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__(258));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHJpbnQuYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EsVSIsInNvdXJjZXMiOlsid2VicGFjazovL2xlc3NvbjQ3X3dlYnBhY2svLi9zcmMvcHJpbnQuanMiXSwic291cmNlc0NvbnRlbnQiOlsiZnVuY3Rpb24gcHJpbnRNZSgpIHtcclxuICBjb25zb2xlLmxvZyhcIkkgZ2V0IGNhbGxlZCBmcm9tIHByaW50LmpzIVwiKTtcclxufVxyXG5cclxucHJpbnRNZSgpOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
